732ffa296283e6d4f663f83780f3e775fa145ebd
Kapil <kapilpalanivel1@gmail.com>
2024-04-13 01:28:28 +0530

    Main page has to be implemented

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,26 +1,29 @@
 import { useRef } from "react";
-import { FaBars, FaLine, FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 import "./NavBar_CSS.css";
-export default function NavBar()
-{
-    const NavRef=useRef();
-    const handleList= ()=>{
+
+export default function NavBar() {
+    const NavRef = useRef();
+
+    const toggleNavbar = () => {
         NavRef.current.classList.toggle("nav");
-    }
-    return(
+    };
+
+    return (
         <header className="header">
-            <h3 className="logo-name">KAPIL</h3>
+            <a href="#" className="logo-name"><h3>KAPIL</h3></a>
             <nav ref={NavRef}>
                 <a href="#">Home</a>
                 <a href="#">Education</a>
+                <a href="#">Projects</a>
                 <a href="#">Personal Blog</a>
-                <a href="#">AboutMe</a>
-                <button className="nav-btn nav-close" onClick={handleList}>
-                    <FaTimes/>
+                <a href="#">About Me</a>
+                <button className="nav-btn nav-close" onClick={toggleNavbar}>
+                    <FaTimes />
                 </button>
             </nav>
-            <button className="nav-btn" onClick={handleList}>
-                <FaBars/>
+            <button className="nav-btn" onClick={toggleNavbar}>
+                <FaBars />
             </button>
         </header>
     );
