b41a1a9620089f2be295115c0c63fa21b6883cec
Kapil <kapilpalanivel1@gmail.com>
2024-04-13 02:38:03 +0530

    Main Page with the Proper Background

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
 import React from 'react';
+import Main from './Components/Home/HomePage';
 function App() {
   return (
     <div className="App">
       <React.Fragment>
         <NavBar/>
+        <Main/>
       </React.Fragment>
     </div>
   );
--- src/Components/Home/HomePage.jsx
+++ src/Components/Home/HomePage.jsx
@@ -1,7 +1,10 @@
+import "./Home.css";
 export default function Main() {
     return(
         <>
-        
+            <div>
+                <h1>Hi, I'm KAPIL</h1>
+            </div>
         </>
     );
 }
